8bb2afdc1b8d2dc5e210023cc1b0385c3e3eeb31
Rocy-June <1048654629@qq.com>
2026-01-20 00:09:09 +0900

    refactor(ui): unify text styles and refine SkillBreakdownView

---
+++ StarResonanceDpsAnalysis.WPF/Views/ControlTestView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace StarResonanceDpsAnalysis.WPF.Views
+{
+    /// <summary>
+    /// Test.xaml 的交互逻辑
+    /// </summary>
+    public partial class ControlTestView : Window
+    {
+        public long TestTotal { get; } = 256_800;
+        public long TestNormalDamage { get; } = 180_000;
+        public long TestCritDamage { get; } = 60_000;
+        public long TestLuckyDamage { get; } = 16_800;
+        public double TestAverage { get; } = 8_560;
+        public long TestHits { get; } = 42;
+        public double TestCritRate { get; } = 0.285;
+        public long TestCritCount { get; } = 12;
+        public long TestLuckyCount { get; } = 4;
+        public double TestLuckyRate { get; } = 0.095;
+
+        public ControlTestView()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
